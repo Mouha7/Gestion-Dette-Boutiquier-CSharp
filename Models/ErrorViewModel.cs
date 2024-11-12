066030ee978c3ca65e0137cebb2e59984cc7d870
Seydina Mouhamed Rassoul Sow <ra.sow25@gmail.com>
2024-11-12 14:06:15 +0000

    Web

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gestion_dette_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
